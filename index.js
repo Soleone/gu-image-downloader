c8a8be161d6a99c8915da9b7f3d4614459653a02
Soleone <dev@soleone.xyz>
2023-10-12 15:36:32 -0400

    Working script to bulk download all images

---
+++ index.js
@@ -0,0 +1,32 @@
+import fetch from "node-fetch";
+import fs from "fs";
+
+const BASE_URL = "https://card.godsunchained.com/?q=4&id=";
+const startId = 2401
+const maxId = 2553
+
+async function downloadImage(id) {
+  const url = `${BASE_URL}${id}`;
+  const response = await fetch(url);
+  const protoDataString = response.headers.get("x-proto-data");
+  const protoData = JSON.parse(protoDataString);
+  const name = protoData.name;
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  const filename = `images/${id}-${name}.jpg`
+  await fs.writeFile(filename, buffer, (error) => { error && console.log(error) })
+  console.log(`Downloaded image ${filename}`)
+}
+
+function downloadImages() {
+  for (let id = startId; id <= maxId; id++) {
+    try {
+      downloadImage(id);
+    } catch (error) {
+      console.log(`Skipping image ${id}`);
+    }
+  }
+}
+
+
+downloadImages();
